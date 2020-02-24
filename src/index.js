3e891df456c7e8dc91f3796bc1415286978f7214
Artsemi Shareika <artsemishareika@gmail.com>
2020-02-24 18:00:41 +0300

    Complete task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+ if (n < 0) {
+     let arr = String(n).split('').reverse();
+    let str = arr.slice(0, arr.length - 1).join('')
+    return Number(str);
+ }
+ let str = String(n).split('').reverse().join('');
+ return Number(str);
 }
